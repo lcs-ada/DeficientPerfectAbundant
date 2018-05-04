3fb62c0bdb58a6e08529d07d614be3a799a933f9
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2018-05-04 07:14:18 -0400

    final version completed

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -9,25 +9,53 @@
 import Foundation
 
 //INPUT
-let n = 24
+var validInput = 0
+//Loop forever until vaild input is found
+while 1 == 1 {
+    
+
+//test 1: ensure input is not nil
+guard let givenInput = readLine()else {
+    continue //go to the top of the loop
+     }
+
+    //test 2: Is it an Integer?
+     guard let givenInteger = Int(givenInput) else {
+     continue// go to the top of the while loop
+     }
+
+    //test 3: Is the integer in the correct range?
+    //less than 1 OR more than 32500
+    if givenInteger < 1 || givenInteger > 32500 {
+    continue //go to top of the while loop
+     }
+
+validInput = givenInteger
+
+break //Important: get us out of the infinite loop
+
+} //END of while loop
+
 
 //PROCESS
 var sum = 1
 //  Check all numbers from 2 to halfway for n
-for i in 2...n/2 {
+for i in 2...validInput/2 {
     print(i)
     //Is i a divisor of n?
-    if n % i == 0 {
+    if validInput % i == 0 {
         //i is a divisor so add to the sum
         sum += i
     }
 }
 
 //OUTPUT
-if sum > n {   
-    print("\(n) is an abundant number.")
-} else if sum < n {
-    print("\(n) is a deficient number.")
-} else if sum == n {
-    print("\(n) is a perfect number.")
+if sum > validInput {
+    print("\(validInput) is an abundant number.")
+} else if sum < validInput {
+    print("\(validInput) is a deficient number.")
+} else if sum == validInput {
+    print("\(validInput) is a perfect number.")
 }
+
+
